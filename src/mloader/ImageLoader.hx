5db1a7392cc645c5425f2c16f6d4ee47e8d57de2
Tomas Brixi <tbrixi@gmail.com>
2014-06-04 12:45:37 +0200

    enable download image from cross domain server

--- src/mloader/ImageLoader.hx
+++ src/mloader/ImageLoader.hx
@@ -60,7 +60,7 @@ class ImageLoader extends LoaderBase<LoadableImage>
 			content = image;
 		}
 		
-		#if !haxe3 untyped #end content.crossOrigin = "Anonymous";
+		//#if !haxe3 untyped #end content.crossOrigin = "Anonymous";
 		content.onload = imageLoad;
 		content.onerror = imageError;
 		content.src = url;
